4431b0d60d9e4c70fbf489a760a9e661785f83b4
stronty <strontium8762aem@gmail.com>
2026-03-25 16:22:09 +0300

    move greeting method from app to cli

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void cli(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games");
+        System.out.print("May I have your name? ");
+        String username = scanner.nextLine();
+        System.out.println("Hello, " + username + "!");
+
+        scanner.close();
+    }
+}
