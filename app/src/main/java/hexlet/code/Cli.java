eb7dd4a432593b789cb43daf20803e468216cea4
stronty <strontium8762aem@gmail.com>
2026-03-28 15:54:35 +0300

    add access to the username

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,14 +3,17 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void cli(){
-        Scanner scanner = new Scanner(System.in);
+    private static String username;
+    public static void cli() {
 
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games");
         System.out.print("May I have your name? ");
-        String username = scanner.nextLine();
+        username = scanner.nextLine();
         System.out.println("\nHello, " + username + "!");
-
-        scanner.close();
+//        scanner.close();
+    }
+    public static String getUsername(){
+        return username;
     }
 }
