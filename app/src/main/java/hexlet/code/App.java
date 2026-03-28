e008d9f0d90d200ce599406934c141efbf06af76
stronty <strontium8762aem@gmail.com>
2026-03-29 02:17:33 +0300

    change project structure

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,30 +3,16 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args){
-        System.out.println("Please enter the game number and press Enter");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
+    public static void main(String[] args) {
 
-        System.out.println("0 - Exit");
+        Engine.gamesList();
 
         System.out.print("Your choice: ");
 
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
+        int userInput = Integer.parseInt(scanner.nextLine());
 
-        switch (Integer.parseInt(userInput)){
-            case 1:
-                Cli.cli();
-                break;
+        Engine.startGame(userInput);
 
-            case 2:
-                Cli.cli();
-                Even.evenGame();
-
-            case 0:
-                scanner.close();
-        }
     }
-
 }
---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,41 @@
+package hexlet.code;
+
+import hexlet.code.games.Calc;
+import hexlet.code.games.Cli;
+import hexlet.code.games.Even;
+
+public class Engine {
+    public static void gamesList(){
+        System.out.println("Please enter the game number and press Enter");
+        String[] gamesList = {
+                "1 - Greet",
+                "2 - Even",
+                "3 - Calc",
+                "0 - Exit"
+        };
+
+        for(var game:gamesList) System.out.println(game);
+    }
+
+    public static void startGame(int gameNum){
+
+       switch (gameNum) {
+           case 1:
+               Cli.cli();
+               return;
+           case 2:
+               Even.evenGame();
+               break;
+           case 3:
+               Calc.calc();
+               break;
+           case 0: default:
+               return;
+       }
+
+    }
+
+
+
+
+}
