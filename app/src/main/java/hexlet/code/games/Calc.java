7703fa2de0163f533191fb543095b5029374d57f
stronty <strontium8762aem@gmail.com>
2026-03-29 02:13:54 +0300

    create calc game

---
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -0,0 +1,48 @@
+package hexlet.code.games;
+
+import java.util.Scanner;
+
+public class Calc {
+    private static String guess;
+
+
+    public static void calc(){
+        Cli.cli();
+        // Rule for the game
+        System.out.println("What is the result of the expression?");
+        //Core mechanic
+
+        // - + *
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        for(var i = 0; i < 3; i++){
+            int randNum1 = (int)(Math.random() * 100);
+            int randNum2 = (int)(Math.random() * 100);
+            int max = 2;
+            int operatorIndex = (int) (Math.random() * (max + 1));
+            String operator;
+
+            int answer;
+            if( operatorIndex == 0){
+                answer = randNum1 + randNum2;
+                operator = "+";
+            }else if( operatorIndex == 1){
+                answer = randNum1 - randNum2;
+                operator = "-";
+            }else{
+                answer = randNum1 * randNum2;
+                operator = "*";
+            }
+
+            System.out.println("Question: " + randNum1 + operator + randNum2);
+            System.out.print("Your answer: ");
+            guess = scanner.nextLine();
+            if (!Game.keepPlaying(guess, Integer.toString(answer))){
+                return;
+            }
+        }
+        Game.winGame();
+    }
+}
