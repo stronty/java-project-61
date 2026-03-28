c1a35a07e8e3b32134ad133f2cd113e29ecb51fb
stronty <strontium8762aem@gmail.com>
2026-03-28 15:53:40 +0300

    add game menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,32 @@
 package hexlet.code;
 
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        Cli.cli();
+        System.out.println("Please enter the game number and press Enter");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+
+        System.out.println("0 - Exit");
+
+        System.out.print("Your choice: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        switch (Integer.parseInt(userInput)){
+            case 1:
+                Cli.cli();
+                break;
+
+            case 2:
+                Cli.cli();
+                Even.evenGame();
+
+            case 0:
+                scanner.close();
+        }
     }
 
 }
