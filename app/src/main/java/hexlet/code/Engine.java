d71611ed25fbf8ddb1c828cb1b7bd83133f04428
stronty <strontium8762aem@gmail.com>
2026-03-31 18:03:40 +0300

    fix checkstyle violations

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,53 +1,57 @@
 package hexlet.code;
 
-import hexlet.code.games.*;
+import hexlet.code.games.Calc;
+import hexlet.code.games.Cli;
+import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
+import hexlet.code.games.PrimeNumbers;
+import hexlet.code.games.Sequence;
 
 public class Engine {
     public static void gamesList(){
         System.out.println("Please enter the game number and press Enter");
         String[] gamesList = {
-                "1 - Greet",
-                "2 - Even",
-                "3 - Calc",
-                "4 - GCD",
-                "5 - Progression",
-                "6 - Prime",
-                "0 - Exit"
+            "1 - Greet",
+            "2 - Even",
+            "3 - Calc",
+            "4 - GCD",
+            "5 - Progression",
+            "6 - Prime",
+            "0 - Exit"
         };
 
         for(var game:gamesList) System.out.println(game);
     }
 
     public static void startGame(int gameNum){
-
-       switch (gameNum) {
-           case 1:
-               Cli.cli();
-               return;
-           case 2:
-               Even.game();
-               break;
-           case 3:
-               Calc.game();
-               break;
-           case 4:
-               GCD.game();
-               break;
-           case 5:
-               Sequence.game();
-               break;
-           case 6:
-               PrimeNumbers.game();
-               break;
-           case 0: default:
-               break;
-       }
+        switch(gameNum){
+            case 1:
+                Cli.cli();
+                return;
+            case 2:
+                Even.game();
+                break;
+            case 3:
+                Calc.game();
+                break;
+            case 4:
+                GCD.game();
+                break;
+            case 5:
+                Sequence.game();
+                break;
+            case 6:
+                PrimeNumbers.game();
+                break;
+            case 0: default:
+                break;
+        }
 
     }
 
 
     public static void loseGame(String guess, String answer){
-        System.out.println("'" + guess + "' is wrong asnwer ;(. " +
+        System.out.println("'" + guess + "' is wrong answer ;(. " +
                 "Correct answer was '" + answer + "'." );
     }
 
