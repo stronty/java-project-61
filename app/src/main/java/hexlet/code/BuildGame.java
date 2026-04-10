faf61e52493ddf8aeab2381f7dde14f62f975a59
stronty <strontium8762aem@gmail.com>
2026-04-10 10:47:26 +0300

    move logic from cli to BuildGame

--- app/src/main/java/hexlet/code/BuildGame.java
+++ app/src/main/java/hexlet/code/BuildGame.java
@@ -8,14 +8,21 @@ import static hexlet.code.games.GameConstants.TOTAL_ROUNDS;
 
 public class BuildGame {
     private static boolean printed = false;
+    private static String username;
 
     public static int buildGame(String rule, String[] gameData, int i) {
         var problem = gameData[0];
         var answer = gameData[1];
         var i1 = i;
 
-        Cli.cli();
+
         if (!printed) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Welcome to the Brain Games!");
+            System.out.print("May I have your name? ");
+            username = scanner.nextLine();
+            System.out.println("Hello, " + username + "!\n");
+
             System.out.println(rule);
             printed = true;
         }
@@ -29,7 +36,7 @@ public class BuildGame {
             System.out.println("Correct!");
             i1++;
             if (i1 == TOTAL_ROUNDS) {
-                System.out.println("\nCongratulations, " + Cli.getUsername() + "!");
+                System.out.println("\nCongratulations, " + username + "!");
             }
         } else {
             i1 = TOTAL_ROUNDS;
@@ -44,6 +51,6 @@ public class BuildGame {
         System.out.println("'" + guess + "' is wrong answer ;(. "
                 + "Correct answer was '" + answer + "'.");
 
-        System.out.println("Let's try again, " + Cli.getUsername() + "!");
+        System.out.println("Let's try again, " + username + "!");
     }
 }
--- app/src/main/java/hexlet/code/games/Cli.java
+++ app/src/main/java/hexlet/code/games/Cli.java
@@ -3,20 +3,15 @@ package hexlet.code.games;
 import java.util.Scanner;
 
 public class Cli {
-    private static String username;
-
     public static void cli() {
-        if (username == null) {
+
             Scanner scanner = new Scanner(System.in);
             System.out.println("Welcome to the Brain Games!");
             System.out.print("May I have your name? ");
-            username = scanner.nextLine();
+            var username = scanner.nextLine();
             System.out.println("Hello, " + username + "!\n");
-        }
-//        scanner.close();
+
+            //        scanner.close();
     }
 
-    public static String getUsername() {
-        return username;
-    }
 }
