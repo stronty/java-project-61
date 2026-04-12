cde9ee89db3ca70da63223f458fad70c9d6edc16
stronty <strontium8762aem@gmail.com>
2026-04-12 14:37:27 +0300

    fix checkstyle complaints
    fix checkstyle complaints

--- app/src/main/java/hexlet/code/games/Cli.java
+++ app/src/main/java/hexlet/code/games/Cli.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 
 public class Cli {
     public static void cli() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        var username = scanner.nextLine();
+        System.out.println("Hello, " + username + "!\n");
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Welcome to the Brain Games!");
-            System.out.print("May I have your name? ");
-            var username = scanner.nextLine();
-            System.out.println("Hello, " + username + "!\n");
-
-            //        scanner.close();
     }
 
 }
